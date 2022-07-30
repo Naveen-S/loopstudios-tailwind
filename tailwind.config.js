3b0a458e601cc259e126c139cb05acd15c4b1729
Naveen <naveens@tekion.com>
2022-07-30 12:28:12 +0530

    Initial commit 1.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      spacing: {
+        x: '32rem',
+      },
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '0.3em',
+      },
+    },
   },
   plugins: [],
 };
