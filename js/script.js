6bf636d8d479a3ddd606b00ae13069fe588b7f7e
Naveen <naveens@tekion.com>
2022-08-01 22:54:45 +0530

    Completed.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('connected');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+btn.addEventListener('click', navToggle);
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('hidden');
+}
